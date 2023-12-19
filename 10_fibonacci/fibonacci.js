7461a8c1c16b366a3f5d72b4dafa1fa68d49f391
gsr142 <gsr142@gmail.com>
2023-12-19 12:27:00 -0800

    finished exercises through 12

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function (int) {
+  let y = parseInt(int);
+  if (y == 0 || y == 1) {
+    return y;
+  } else if (y < 0) {
+    return "OOPS";
+  } else {
+    let fibArr = [1, 1];
+    while (fibArr.length < y) {
+      let x = fibArr[fibArr.length-1] + fibArr[fibArr.length-2];
+      fibArr.push(x);
+    }
+    return fibArr[fibArr.length-1]
+  }
 };
 
 // Do not edit below this line
